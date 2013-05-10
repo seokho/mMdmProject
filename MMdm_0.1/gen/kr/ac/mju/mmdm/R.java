75a319a110b62df5df0cb6d627a9c62fe42d5c06
seok-ho <osh8777@naver.com>
2013-05-11 03:35:05 +0900

    simple taskmanager

--- MMdm_0.1/gen/kr/ac/mju/mmdm/R.java
+++ MMdm_0.1/gen/kr/ac/mju/mmdm/R.java
@@ -20,11 +20,12 @@ public final class R {
         public static final int gridview=0x7f07000c;
         public static final int id=0x7f070005;
         public static final int imageView=0x7f070001;
+        public static final int killbtn=0x7f07000d;
         public static final int login=0x7f070007;
         public static final int login_form=0x7f070004;
         public static final int login_status=0x7f070002;
         public static final int login_status_message=0x7f070003;
-        public static final int menu_forgot_password=0x7f07000d;
+        public static final int menu_forgot_password=0x7f07000e;
         public static final int nonsign_in_button=0x7f07000a;
         public static final int password=0x7f070006;
         public static final int regist_in_button=0x7f07000b;
--- MMdm_0.1/src/kr/ac/mju/mmdm/taskmanager/TaskManagerActivity.java
+++ MMdm_0.1/src/kr/ac/mju/mmdm/taskmanager/TaskManagerActivity.java
@@ -1,8 +1,18 @@
 package kr.ac.mju.mmdm.taskmanager;
 
+import java.util.List;
+
 import kr.ac.mju.mmdm.R;
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningAppProcessInfo;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 
@@ -12,6 +22,27 @@ public class TaskManagerActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_task);
 		
+		Button btn1 = (Button)findViewById(R.id.killbtn);
+		
+		btn1.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+				PackageManager pkManager = getPackageManager();
+				List<RunningAppProcessInfo> runningAppList = manager.getRunningAppProcesses();
+
+				for (RunningAppProcessInfo info : runningAppList) {
+				    // info.processName
+					String process = info.processName;
+					manager.killBackgroundProcesses(process);
+				}
+				
+				Toast.makeText(getBaseContext(), "Killed", 0).show();
+			}
+			
+		});
 	}
 	
 }
